385f41ece662c4ee26d3009dc69dd1ba0bd8033c
Tatao_ <kirarinice@gmail.com>
2020-08-26 21:58:34 +0900

    add waitMs, showPixel function　※オブジェクト左辺値参照　要動作確認

--- lib/MechanismTask.hpp
+++ lib/MechanismTask.hpp
@@ -5,12 +5,14 @@
 #include "../lib/ServoDriver.hpp"
 #include "../lib/AntiChattering.hpp"
 #include "../lib/AnalogIn.hpp"
+#include "../lib/Adafruit_NeoPixel.h"
 
 class MechanismTask
 {
 private:
     FlagSet &flag_set;
     MotorDriver MotorSet[4] = {motor1, motor2, motor3, motor4};
+    Adafruit_NeoPixel strip1, strip2;
     void work1();
     void work2();
     void work3();
@@ -21,6 +23,9 @@ private:
     void apply_emergency_state();
     void apply_pwms();
 
+    bool waitMs(uint16_t);
+    void showPixels(Adafruit_NeoPixel &, uint32_t);
+
 public:
     MechanismTask() = delete;
     MechanismTask(FlagSet &);
@@ -28,7 +33,15 @@ public:
 
     void work();
 };
-inline MechanismTask::MechanismTask(FlagSet &flag_set_) : flag_set(flag_set_){};
+
+inline MechanismTask::MechanismTask(FlagSet &flag_set_)
+    : flag_set(flag_set_),
+      strip1(NEOPIXEL1_PROPS.num_pixels, NEOPIXEL1_PROPS.pin, NEO_GRB + NEO_KHZ800),
+      strip2(NEOPIXEL2_PROPS.num_pixels, NEOPIXEL2_PROPS.pin, NEO_GRB + NEO_KHZ800)
+{
+    strip1.begin();
+    strip2.begin();
+};
 
 inline void MechanismTask::apply_motor_stop()
 {
@@ -53,4 +66,24 @@ inline void MechanismTask::apply_emergency_state()
         return;
     this->apply_motor_stop();
     return;
+}
+
+inline bool MechanismTask::waitMs(uint16_t times)
+{
+    static unsigned long long prevLaunched = 0;
+    if ((millis() - prevLaunched) > times)
+    {
+        prevLaunched = millis();
+        return 1;
+    }
+    return 0;
+}
+
+inline void MechanismTask::showPixels(Adafruit_NeoPixel &strip, uint32_t color)
+{
+    for (uint16_t i = 0; i < strip.numPixels(); i++)
+    {
+        strip.setPixelColor(i, color);
+        strip.show();
+    }
 }
